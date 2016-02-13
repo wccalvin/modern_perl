5a7c0c0c636a526d599980e28042bf006e071b9b
Clayton William <clayton.calvin@gmail.com>
2016-02-13 09:40:55 -0500

    mods to some examples

--- basics/quotes.pl
+++ basics/quotes.pl
@@ -5,8 +5,9 @@ use warnings;
 use autodie;
 
 my $var1 = "Hello World.";
+my $var2 = q[This is so much fun... $var1];
 
 say qq[This is so much fun.... $var1];
-my $var2 = q[This is so much fun... $var1];
 say split(" ", $var2);
 say qw[This is so much fun.... $var1];
+
