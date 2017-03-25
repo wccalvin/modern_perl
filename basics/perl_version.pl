960b3880db6bbefa611b3e7efe1779e691c4263c
Clayton William <clayton.calvin@gmail.com>
2017-03-25 16:20:48 -0400

    few more examples

--- basics/perl_version.pl
+++ basics/perl_version.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-use strict;
+use v5.10;
 use Data::Dumper;
 
 my $version = $];
--- basics/quotes.pl
+++ basics/quotes.pl
@@ -10,4 +10,3 @@ my $var2 = q[This is so much fun... $var1];
 say qq[This is so much fun.... $var1];
 say split(" ", $var2);
 say qw[This is so much fun.... $var1];
-
